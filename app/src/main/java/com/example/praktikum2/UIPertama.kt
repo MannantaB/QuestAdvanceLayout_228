dd4eed06ee43d2b07a9c446741c6aeaf4719afaf
MannantaB <mannanta18@gmail.com>
2025-10-23 18:35:12 +0700

    memperbaiki letak code phone dan alamat

--- app/src/main/java/com/example/praktikum2/UIPertama.kt
+++ app/src/main/java/com/example/praktikum2/UIPertama.kt
@@ -94,16 +94,16 @@ fun ActivitasPertama(modifier: Modifier){
                         modifier = Modifier.padding(top = 15.dp)
                     )
                     Text(
-                        text = stringResource(id = R.string.phone),
-                        fontSize = 20.sp,
-                        color = Color.White,
-                        modifier = Modifier.padding(top = 15.dp)
-                    )
-                    Text(
                         text = stringResource(id = R.string.alamat),
                         fontSize = 20.sp,
                         color = Color.Yellow,
-                        modifier = Modifier.padding(top = 10.dp, bottom = 15.dp)
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.phone),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp, bottom = 15.dp)
                     )
                 }
             }
@@ -134,16 +134,16 @@ fun ActivitasPertama(modifier: Modifier){
                         modifier = Modifier.padding(top = 15.dp)
                     )
                     Text(
-                        text = stringResource(id = R.string.phone),
-                        fontSize = 20.sp,
-                        color = Color.White,
-                        modifier = Modifier.padding(top = 15.dp)
-                    )
-                    Text(
                         text = stringResource(id = R.string.alamat),
                         fontSize = 20.sp,
                         color = Color.Yellow,
-                        modifier = Modifier.padding(top = 10.dp, bottom = 15.dp)
+                        modifier = Modifier.padding(top = 10.dp)
+                    )
+                    Text(
+                        text = stringResource(id = R.string.phone),
+                        fontSize = 20.sp,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 15.dp, bottom = 15.dp)
                     )
                 }
             }
