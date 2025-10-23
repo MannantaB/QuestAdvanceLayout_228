bc9fe640cf1de0e0bd7cf77df2d028d2f3892cbb
MannantaB <mannanta18@gmail.com>
2025-10-23 08:30:09 +0700

    Menambahkan fungsi @composable ActivitasPertama

---
+++ app/src/main/java/com/example/praktikum2/UIPertama.kt
@@ -0,0 +1,10 @@
+package com.example.praktikum2
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import java.lang.reflect.Modifier
+
+@Composable
+fun ActivitasPertama(modifier: Modifier){
+
+}
