320b926be7bd3ef2ceb63a756d45fd91a2d454d1
MannantaB <mannanta18@gmail.com>
2025-10-23 09:05:22 +0700

    Menambahkan layout Box dengan ukuran penuh (fillMaxSize)

--- app/src/main/java/com/example/praktikum2/UIPertama.kt
+++ app/src/main/java/com/example/praktikum2/UIPertama.kt
@@ -1,6 +1,7 @@
 package com.example.praktikum2
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -67,6 +68,10 @@ fun ActivitasPertama(modifier: Modifier){
                 }
             }
         }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        )
 
     }
 }
