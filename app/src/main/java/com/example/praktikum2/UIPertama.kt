1cf9065ed532dfb132dd3c61fc465d1ae52b0e95
MannantaB <mannanta18@gmail.com>
2025-10-23 18:27:08 +0700

    menampilkan gambar logo dan add spacer

--- app/src/main/java/com/example/praktikum2/UIPertama.kt
+++ app/src/main/java/com/example/praktikum2/UIPertama.kt
@@ -117,7 +117,15 @@ fun ActivitasPertama(modifier: Modifier){
             )) {
             Row(
                 verticalAlignment = Alignment.CenterVertically
-            ){}
+            ){
+                val gambar = painterResource(id = R.drawable.logo)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier.size(size = 100.dp).padding(all = 5.dp)
+                )
+                Spacer(modifier = Modifier.width(width = 30.dp))
+            }
         }
 
         Box(
