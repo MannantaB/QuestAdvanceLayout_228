a1bfdedff71f73d093744619d37c5570562fdc3c
MannantaB <mannanta18@gmail.com>
2025-10-23 08:40:38 +0700

    Menambahkan komponen Text untuk menampilkan string univ dengan font 22sp

--- app/src/main/java/com/example/praktikum2/UIPertama.kt
+++ app/src/main/java/com/example/praktikum2/UIPertama.kt
@@ -21,5 +21,9 @@ fun ActivitasPertama(modifier: Modifier){
         Text(text = stringResource(id = R.string.prodi),
             fontSize = 35.sp,
             fontWeight = FontWeight.Bold)
+        Text(text = stringResource(id = R.string.univ),
+            fontSize = 22.sp)
+
+
     }
 }
